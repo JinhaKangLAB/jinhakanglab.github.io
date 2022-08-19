f1148b1cb7e5b331fbd4ac8b2ab0c7fa4327e5ef
taewon.lee <taewon.lee@navercorp.com>
2022-08-19 13:04:02 +0900

    lint 적용 & div row 생성 & article 가로배치 & script를 </body> 태그 위로 변경

--- js/ie.js
+++ js/ie.js
@@ -1,22 +1,18 @@
 const box = document.querySelector("#box");
 const article = document.querySelectorAll("article");
 
-for(let i=0;i<article.length;i++){
-
-article[0].addEventListener("mouseenter",()=>{
- box.classList.add("caseOne");
+article[0].addEventListener("mouseenter", () => {
+    box.classList.add("caseOne");
 });
 
-article[0].addEventListener("mouseleave",()=>{
+article[0].addEventListener("mouseleave", () => {
     box.classList.remove("caseOne");
-   });
+});
 
-   
-article[1].addEventListener("mouseenter",()=>{
+article[1].addEventListener("mouseenter", () => {
     box.classList.add("caseTwo");
-   });
-   
-   article[1].addEventListener("mouseleave",()=>{
-       box.classList.remove("caseTwo");
-      });
-}
+});
+
+article[1].addEventListener("mouseleave", () => {
+    box.classList.remove("caseTwo");
+});
